dc29e4d98bc5384dba305f10fb667a670086ff9e
Ben Armston <ben.armston@googlemail.com>
2011-05-25 21:43:46 +0100

    Replace use of Text::Blaze::GangliaXml with xmlgen
    Use of thrid party library is much nicer than my own poorly implemented
    solution.

--- src/Hmond/Server.hs
+++ src/Hmond/Server.hs
@@ -1,25 +1,16 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeSynonymInstances #-}
---  {-# LANGUAGE FlexibleInstances #-}
---  {-# LANGUAGE UndecidableInstances #-}
-
-module Hmond.Server (start) where
+module Hmond.Server (start, genAttrList, sampleText) where
 
 import           Control.Concurrent
 import           Control.Monad
 import           Data.Time.Clock (UTCTime, getCurrentTime)
---  import           Data.Time.Clock.POSIX (POSIXTime, getPOSIXTime)
 import           Data.Time.Format
 import           Network
 import           System.IO
 import           System.Locale
---  import           System.IO.UTF8           as UTF8
 
---  import Text.Blaze.Renderer.Utf8 (renderHtml)
-import Text.Blaze.Renderer.Pretty (renderHtml)
+import qualified Data.ByteString as BS
 
-import Text.Blaze.GangliaXml
-import Text.Blaze.GangliaXml.Attributes
+import Text.XML.Generator
 
 start :: IO ()
 start = withSocketsDo $ do
@@ -34,20 +25,36 @@ start = withSocketsDo $ do
 
 handleClient :: Handle -> IO ()
 handleClient handle = do
-    --  now <- getPOSIXTime
     now <- getCurrentTime
-    --  UTF8.hPutStrLn handle sampleText
-    hPutStrLn handle $ sampleText now
+    BS.hPutStrLn handle $ sampleText now
     hClose handle
 
 
-sampleText :: UTCTime -> String
-sampleText now = renderHtml $ do
-    ganglia_xml ! source "hmond" ! version "2.5.7" $ do
-        cluster ! localtime (toValue now) ! owner "owner" ! latlong "unknown" ! url "unknown" ! name "unspecified" $ do
-            host ! ip "192.168.1.1" ! reported (toValue now) ! tn "50" ! tmax "60" ! name "comp02.vm.concurrent-thinking.com" ! dmax "600" $ do
-                "It works"
+sampleText :: (FormatTime t, XmlOutput x) => t -> x
+sampleText now = xrender $ doc defaultDocInfo $
+    xelem "GANGLIA_XML" $ genAttrList [ ("source", "hmond")
+                                      , ("version", "2.5.7")
+                                      ] <#>
+        (xelem "CLUSTER" $ genAttrList [ ("owner", "owner")
+                                       , ("localtime", localtime now)
+                                       , ("latlong", "unknown")
+                                       , ("url", "unknown")
+                                       , ("name", "unspecified")
+                                       ] <#>
+            (xelems $ map (\(name, ip) -> xelem "HOST" $ genAttrList [ ("ip", ip)
+                                                                     , ("name", name)
+                                                                     , ("reported", localtime now)
+                                                                     , ("tn", "50")
+                                                                     , ("tmax", "60")
+                                                                     , ("dmax", "600")
+                                                                     ] <#>
+                xtext "It works") hosts))
+
+  where localtime = formatTime defaultTimeLocale "%s"
+        hosts = [ ("comp00.vm.concurrent-thinking.com", "192.168.1.1")
+                , ("comp01.vm.concurrent-thinking.com", "192.168.1.2")
+                ]
 
 
-instance ToValue UTCTime where
-    toValue = stringValue . formatTime defaultTimeLocale "%s"
+genAttrList :: [(String, String)] -> Xml Attr
+genAttrList = xattrs . map (uncurry xattr)
