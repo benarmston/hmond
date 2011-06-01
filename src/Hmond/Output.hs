6a6f0909e145b990177554260956b588cf08f880
Ben Armston <ben.armston@googlemail.com>
2011-06-01 01:01:54 +0100

    Use uppercase names for attributes like Ganglia

--- src/Hmond/Output.hs
+++ src/Hmond/Output.hs
@@ -12,34 +12,34 @@ import Hmond.Metrics
 
 generateXML :: (FormatTime t, XmlOutput x) => t -> [Host] -> x
 generateXML now hosts = xrender $ doc defaultDocInfo $
-    xelem "GANGLIA_XML" $ genAttrList [ ("source", "hmond")
-                                      , ("version", "2.5.7")
+    xelem "GANGLIA_XML" $ genAttrList [ ("SOURCE", "hmond")
+                                      , ("VERSION", "2.5.7")
                                       ] <#>
-        (xelem "CLUSTER" $ genAttrList [ ("owner", "owner")
-                                       , ("localtime", localtime now)
-                                       , ("latlong", "unknown")
-                                       , ("url", "unknown")
-                                       , ("name", "unspecified")
+        (xelem "CLUSTER" $ genAttrList [ ("OWNER", "owner")
+                                       , ("LOCALTIME", localtime now)
+                                       , ("LATLONG", "unknown")
+                                       , ("URL", "unknown")
+                                       , ("NAME", "unspecified")
                                        ] <#>
             (xelems $ map (hostXml now) hosts ))
 
 
 hostXml :: FormatTime t => t -> Host -> Xml Elem
-hostXml now host = xelem "HOST" $ genAttrList [ ("ip", hostIP host)
-                                               , ("name", hostname host)
-                                               , ("reported", localtime now)
-                                               , ("tn", "50")
-                                               , ("tmax", "60")
-                                               , ("dmax", "600")
-                                               ] <#>
+hostXml now host = xelem "HOST" $ genAttrList [ ("IP", hostIP host)
+                                              , ("NAME", hostname host)
+                                              , ("REPORTED", localtime now)
+                                              , ("TN", "50")
+                                              , ("TMAX", "60")
+                                              , ("DMAX", "600")
+                                              ] <#>
                         (xelems $ map metricXml (hostMetrics host))
 
 
 metricXml :: Metric -> Xml Elem
-metricXml metric = xelem "METRIC" $ genAttrList [ ("name", metricName metric)
-                                                 , ("type", metricType metric)
-                                                 , ("val" , show . metricValue $ metric)
-                                                 ]
+metricXml metric = xelem "METRIC" $ genAttrList [ ("NAME", metricName metric)
+                                                , ("TYPE", metricType metric)
+                                                , ("VAL" , show . metricValue $ metric)
+                                                ]
 
 
 localtime :: (FormatTime t) => t -> String
