e725e163b8a8ac1c724811b768ac5314a6c29646
Ben Armston <ben.armston@googlemail.com>
2012-04-29 21:22:09 +0100

    Effort to use AnyMetric as a type synonym
    It doesn't compile though.

--- src/Experiment/Main.hs
+++ src/Experiment/Main.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ImpredicativeTypes #-}
 {-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 
 module Main where
@@ -10,37 +12,18 @@ import Control.Monad(forM_)
 
 -- | Types
 
-data NumericMetricValue
-data StringMetricValue
-
 data MetricValue a where
-    MtInt    :: Int    -> MetricValue NumericMetricValue
-    MtDouble :: Double -> MetricValue NumericMetricValue
-    MtString :: String -> MetricValue StringMetricValue
+    MtInt    :: Int    -> MetricValue Int
+    MtDouble :: Double -> MetricValue Double
+    MtString :: String -> MetricValue String
 
 instance Show (MetricValue a) where
     show (MtString s)    = s
     show (MtInt i)       = show i
     show (MtDouble d)    = show d
 
-class IsMetric a where
-    -- This doesn't work. The type b, is precisely the information I'm trying
-    -- to hide in order create a homogeneous list of Metrics.
-    getMetric :: a -> Metric b
-    --  metricValue :: a -> MetricValue b
-    --  metricValueGen :: a -> ValueGenerator b
+type AnyMetric = forall a. Metric a
 
-instance IsMetric (Metric a) where
-    getMetric = id
-    --  metricValue m    = metricValue_ m
-    --  metricValueGen m = metricValueGen_ m
-
-data AnyMetric = forall a. (IsMetric a) => AnyMetric a
-
-instance IsMetric AnyMetric where
-    getMetric (AnyMetric m) = getMetric m
-    --  metricValue (AnyMetric a)    = metricValue a
-    --  metricValueGen (AnyMetric a) = metricValueGen a
 
 data ValueGenerator a = ValueGenerator { runGenerator :: (MetricValue a, ValueGenerator a) }
 
@@ -48,12 +31,12 @@ data Metric a = Metric { metricValue_    :: MetricValue a
                        , metricValueGen_ :: ValueGenerator a
                        }
 
-data Host a = Host { hostMetrics :: [AnyMetric] }
+data Host = Host { hostMetrics :: [AnyMetric] }
 
 
-makeHost = Host numMetrics -- ::  Host NumericMetricValue
---  makeHost = Host stringMetrics -- ::  Host StringMetricValue
---  makeHost = Host mixedMetrics ::  Host ?
+makeHost = Host numMetrics
+--  makeHost = Host stringMetrics
+--  makeHost = Host mixedMetrics
 
 numMetrics ::  [AnyMetric]
 numMetrics = [ makeMetric $ fixedGenerator $ MtInt 1
@@ -76,24 +59,23 @@ stringMetrics = [ makeMetric $ fixedGenerator $ MtString "Bob"
 --  mixedMetrics = numMetrics ++ stringMetrics
 
 makeMetric ::  ValueGenerator a -> AnyMetric
-makeMetric vg = AnyMetric $ Metric { metricValue_ = value
-                                   , metricValueGen_ = vg
-                                   }
+makeMetric vg = Metric { metricValue_ = value
+                       , metricValueGen_ = vg
+                       }
   where value = fst . runGenerator $ vg
 
 
 
 -- | Processing functions
 
-runMetrics ::  Host a -> Host a
+runMetrics ::  Host -> Host
 runMetrics host = host { hostMetrics = newMetrics}
     where newMetrics = map runMetric $ hostMetrics host
 
 
 runMetric :: AnyMetric -> AnyMetric
-runMetric anyMetric = AnyMetric $ newMetric (metricValueGen_ m)
+runMetric m = newMetric (metricValueGen_ m)
   where
-    m = getMetric anyMetric
     newMetric g = let (val, gen) = runGenerator g in
                       m { metricValue_ = val
                         , metricValueGen_ = gen}
@@ -106,16 +88,14 @@ fixedGenerator i = ValueGenerator fixedVal
     where fixedVal = (i, fixedGenerator i)
 
 
--- This doesn't work for MtString. But there is no compile time check.
-decrementingGenerator :: MetricValue NumericMetricValue -> ValueGenerator NumericMetricValue
+decrementingGenerator :: Num a => MetricValue a -> ValueGenerator a
 decrementingGenerator (MtInt i)    = ValueGenerator decrement
     where decrement = (MtInt i, decrementingGenerator $ MtInt (i - 1))
 decrementingGenerator (MtDouble i) = ValueGenerator decrement
     where decrement = (MtDouble i, decrementingGenerator $ MtDouble (i - 1))
 
 
--- This only works for MtString. But there is no compile time check.
-caseTogglingGenerator :: MetricValue StringMetricValue -> ValueGenerator StringMetricValue
+caseTogglingGenerator :: MetricValue String -> ValueGenerator String
 caseTogglingGenerator (MtString s) = ValueGenerator (MtString s, caseTogglingGenerator toggled)
     where toggled = MtString $ if all isUpper s
                                 then map toLower s
@@ -129,9 +109,9 @@ main = do
     printHostVals makeHost
 
 
-printHostVals :: Host a -> IO ()
+printHostVals :: Host -> IO ()
 printHostVals host = do
     forM_ (hostMetrics host) $ \metric ->
-        putStr $ show (metricValue_ $ getMetric metric) ++ " "
+        putStr $ show (metricValue_ $ metric) ++ " "
     putStrLn ""
     printHostVals $ runMetrics host
