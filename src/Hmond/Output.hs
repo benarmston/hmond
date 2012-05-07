bba76d98cc62ce58477ab5418517f53407c59979
Ben Armston <ben.armston@googlemail.com>
2012-05-07 21:42:40 +0100

    Tidy up XML generation
    Each entity has its own method. The attribute generating helper has been
    renamed genAttrList -> attributes.

--- src/Hmond/Output.hs
+++ src/Hmond/Output.hs
@@ -13,48 +13,60 @@ import Text.XML.Generator
 import Hmond.Types
 
 generateXML :: (FormatTime t, XmlOutput x) => t -> ClusterInfo -> [Host] -> x
-generateXML now ClusterInfo{..} hosts = xrender $ doc defaultDocInfo $
-    xelem "GANGLIA_XML" $ genAttrList [ ("VERSION", "2.5.7")
-                                      , ("SOURCE", "hmond")
-                                      ] <#>
-        (xelem "CLUSTER" $ genAttrList [ ("NAME", clName)
-                                       , ("OWNER", clOwner)
-                                       , ("LATLONG", clLatlong)
-                                       , ("URL", clUrl)
-                                       , ("LOCALTIME", localtime now)
-                                       ] <#>
-            (xelems $ map (hostXml now) hosts ))
+generateXML now clusterInfo hosts = xrender $ doc defaultDocInfo $
+    xelem "GANGLIA_XML" $
+        attributes [ ("VERSION", "2.5.7")
+                   , ("SOURCE", "hmond")
+                   ] <#>
+                       clusterXml now clusterInfo hosts
+
+
+clusterXml :: FormatTime t => t -> ClusterInfo -> [Host] -> Xml Elem
+clusterXml now ClusterInfo{..} hosts =
+    xelem "CLUSTER" $
+        attributes [ ("NAME", clName)
+                   , ("OWNER", clOwner)
+                   , ("LATLONG", clLatlong)
+                   , ("URL", clUrl)
+                   , ("LOCALTIME", localtime now)
+                   ] <#>
+                       (xelems $ map (hostXml now) hosts)
 
 
 hostXml :: FormatTime t => t -> Host -> Xml Elem
-hostXml now Host{..} = xelem "HOST" $ genAttrList [ ("NAME", hostname)
-                                                  , ("IP", hostIP)
-                                                  , ("LOCATION", "unknown")
-                                                  , ("REPORTED", localtime now)
-                                                  , ("TN", "50")
-                                                  , ("TMAX", "60")
-                                                  , ("DMAX", "600")
-                                                  -- , ("GMOND_STARTED", localtime hmondStartTime)
-                                                  ] <#>
-                        (xelems $ map (\(MkM mr _) -> metricXml mr) (hostMetrics))
+hostXml now Host{..} =
+    xelem "HOST" $
+        attributes [ ("NAME", hostname)
+                   , ("IP", hostIP)
+                   , ("LOCATION", "unknown")
+                   -- XXX This should probably be when the metric was collected.
+                   , ("REPORTED", localtime now)
+                   , ("TN", "50")
+                   , ("TMAX", "60")
+                   , ("DMAX", "600")
+                   -- , ("GMOND_STARTED", localtime hmondStartTime)
+                   ] <#>
+                       (xelems $ map (\(MkM mr _) -> metricXml mr) (hostMetrics))
 
 
 metricXml :: MetricRecord a -> Xml Elem
-metricXml mr@MkMr{..} = xelem "METRIC" $ genAttrList [ ("NAME", mrName)
-                                                     , ("VAL" , show mrValue)
-                                                     , ("TYPE", mrType mr)
-                                                     , ("UNITS", fromMaybe "" mrUnits)
-                                                     , ("TN", show mrTN)
-                                                     , ("TMAX", show mrTMAX)
-                                                     , ("DMAX", show mrDMAX)
-                                                     , ("SLOPE", show mrSlope)
-                                                     , ("SOURCE", "gmond")
-                                                     ]
+metricXml mr@MkMr{..} =
+    xelem "METRIC" $
+        attributes [ ("NAME", mrName)
+                   , ("VAL" , show mrValue)
+                   , ("TYPE", mrType mr)
+                   , ("UNITS", fromMaybe "" mrUnits)
+                   , ("TN", show mrTN)
+                   , ("TMAX", show mrTMAX)
+                   , ("DMAX", show mrDMAX)
+                   , ("SLOPE", show mrSlope)
+                   , ("SOURCE", "gmond")
+                   ]
 
 
 localtime :: (FormatTime t) => t -> String
 localtime = formatTime defaultTimeLocale "%s"
 
 
-genAttrList :: [(String, String)] -> Xml Attr
-genAttrList = xattrs . map (uncurry xattr)
+attributes :: [(String, String)] -> Xml Attr
+attributes = xattrs . map (uncurry xattr)
